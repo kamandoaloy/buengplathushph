4f543d4a5d7eb201f1c92e35e367918ab0cd12e9
kamandoaloy <39230543+kamandoaloy@users.noreply.github.com>
2025-05-28 04:42:29 +0700

    Add files via upload

---
+++ menubar.php
@@ -0,0 +1,72 @@
+  <header id="header" class="header d-flex align-items-center sticky-top">
+    <div class="container position-relative d-flex align-items-center justify-content-between">
+
+      <a href="index.php" class="logo d-flex align-items-center me-auto me-xl-0">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1 class="sitename">รพ.สต.บ้านบึงปลาทู</h1>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="index.php" class="active">หน้าหลัก</a></li>
+          <li><a href="#">Dashboard</a></li>
+          <li class="dropdown"><a href="#"><span>เกี่ยวกับ รพ.สต.</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="vision.php">วิสัยทัศ/พันธกิจ</a></li>
+              <li><a href="category.html">ข้อมูลทั่วไป</a></li>
+              <li><a href="category.html">บุคคลากร</a></li>
+              <li class="dropdown"><a href="#"><span>แผนยุธศาสตร์</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="single-post.html">ยุทธศาสตร์ประเทศ</a></li>
+                  <li><a href="single-post.html">ยุทธศาสตร์หน่วยงาน</a></li>
+                  <li><a href="single-post.html">แผนปฏิบัติงาน/โครงการประจำปี</a></li>
+                  <li><a href="single-post.html">แผนการใช้เงินประจำปี</a></li>
+                </ul>
+              <li><a href="category.html">การให้บริการ</a></li>
+              <li><a href="single-post.html">มาตรฐานหน่วยบริการปฐมภูมิ</a></li>
+              <li><a href="category.html">ประเมินความพึงพอใจ</a></li>
+              <li><a href="category.html">ร้องทุกข์/แจ้งข่าว</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>ข้อมูลสาธารณสุข</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="category.html">ประชากรและการปกครอง</a></li>
+              <li><a href="category.html">สถิติชีพ</a></li>
+              <li><a href="category.html">ภาวการณ์เกิดโรค</a></li>
+              <li class="dropdown"><a href="#"><span>การดำเนินงานตามตัวชีวัด</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="about.html">ด้านการส่งเสริมสุขภาพ</a></li>
+                  <li><a href="about.html">ด้านการเฝ้าระวังและป้องกันการเกิดโรค</a></li>
+                  <li><a href="about.html">ด้านการรักษาพยาบาล</a></li>
+                  <li><a href="about.html">ด้านการฟื้นฟู</a></li>
+                  <li><a href="about.html">ด้านคุ้มครองผู้บริโภค</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>ภาคีเครือข่าย</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="single-post.html">ชุนชนของเรา</a></li>
+                  <li><a href="single-post.html">ชมรม อสม.</a></li>
+                  <li><a href="single-post.html">ชมรม ผู้สูงอายุ</a></li>
+                </ul>
+          <li class="dropdown"><a href="#"><span>ข่าวสาร/บทความ</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="single-post.html">ประชาสัมพันธ์</a></li>
+                  <li><a href="single-post.html">จัดซื้อ/จัดจ้าง</a></li>
+                  <li><a href="single-post.html">บทความ</a></li>
+                </ul>
+          <li><a href="contact.html">ติดต่อเรา</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+      <div class="header-social-links">
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+      </div>
+
+    </div>
+  </header>
+
+  <main class="main">
---
+++ vision.php
@@ -0,0 +1,78 @@
+<?php include 'header.php'; ?>
+<?php include 'menubar.php'; ?>
+
+  <main class="main">
+
+    <!-- Page Title -->
+    <div class="page-title">
+      <div class="container d-lg-flex justify-content-between align-items-center">
+        <h1 class="mb-2 mb-lg-0">วิสัยทัศน์และพันธกิจของโรงพยาบาลส่งเสริมสุขภาพตำบล บ้านบึงปลาทู</h1>
+        <nav class="breadcrumbs">
+          <ol>
+            <li><a href="index.php">หน้าหลัก</a></li>
+            <li class="current">วิสัยทัศ/พันธกิจ</li>
+          </ol>
+        </nav>
+      </div>
+    </div><!-- End Page Title -->
+
+    <!-- About Section -->
+    <section id="about" class="about section">
+
+      <div class="container">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-6 content" data-aos="fade-up" data-aos-delay="100">
+            <p class="who-we-are"></p>
+            <h3>วิสัยทัศน์</h3>
+            <p class="fst-italic">
+              “องค์กรดีมีมาตรฐาน  บริการผสมผสานแบบองค์รวม  ชุมชนสุขภาพดีแบบมีส่วนร่วม”
+            </p>
+            <h3>พันธกิจ</h3>
+            <p class="fst-italic">
+              <ol>
+                <li><span>พัฒนาระบบการให้บริการและสถานที่ทำงานให้ผ่านมาตรฐาน พร้อมให้บริการผสมผสานประกอบด้วย การสร้างสุขภาพ การป้องกันโรค การรักษาพยาบาล การฟื้นฟูสภาพ</span></li>
+                <li><span>ประชาชนมีสุขภาพดีทั้งร่างกายและจิตใจโดยเน้นการให้บริการใกล้บ้านใกล้ใจด้วยหัวใจความเป็นมนุษย์</span></li>
+                <li><span>สนับสนุนและเพิ่มขีดความสามารถในชุมชน องค์กรปกครองส่วนท้องถิ่นและองค์กรต่าง ๆ  มีส่วนร่วมรับผิดชอบตามความเหมาะสมกับสภาพปัญหาสาธารณสุขและสิ่งแวดล้อม</span></li>
+              </ol>
+            </p>
+                        </p>
+            <h3>วัฒนธรรมขององค์กร</h3>
+            <p class="fst-italic">
+              <ul>
+                <li><i class="bi bi-check-circle"></i> <span>W  :  WILLINGNESS เต็มใจบริการทั้งในและนอกสถานบริการ</span></li>
+                <li><i class="bi bi-check-circle"></i> <span>O  :  OPENNESS เปิดใจยอมรับคำวิพากษ์วิจารณ์เพื่อปรับปรุงให้ดีขึ้น</span></li>
+                <li><i class="bi bi-check-circle"></i> <span>R  :  RESIBILITY ความรับผิดชอบต่อหน้าที่ที่ได้รับมอบหมาย</span></li>
+                <li><i class="bi bi-check-circle"></i> <span>K  :  KNOWLEDGE MANAGEMENT การจัดการความรู้แบบบูรณาการ</span></li>              
+              </ul>
+            </p>
+          </div>
+
+          <div class="col-lg-6 about-images" data-aos="fade-up" data-aos-delay="200">
+            <div class="row gy-4">
+              <div class="col-lg-6">
+                <img src="assets/img/about-company-1.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="col-lg-6">
+                <div class="row gy-4">
+                  <div class="col-lg-12">
+                    <img src="assets/img/about-company-2.jpg" class="img-fluid" alt="">
+                  </div>
+                  <div class="col-lg-12">
+                    <img src="assets/img/about-company-3.jpg" class="img-fluid" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- /About Section -->
+
+  </main>
+
+  <?php include 'footer.php'; ?>
